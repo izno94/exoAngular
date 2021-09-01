2fc5b48e6a770771931b647b7f5d47fbca29193f
izno94 <iznosc2@gmail.com>
2021-09-01 12:25:32 +0200

    add function

--- src/app/imc-cal/imc-cal.component.ts
+++ src/app/imc-cal/imc-cal.component.ts
@@ -6,11 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./imc-cal.component.css']
 })
 export class ImcCalComponent implements OnInit {
- 
-   
+     
+     poids: any = [];
+     taille: any = [];
+     result: any = [];
+     
+
   constructor() { }
+  
 
   ngOnInit() {
+  
   }
+  onClick(){ 
+    this.result = console.log(this.poids + this.taille)
+  } 
 
+  onKey(event: any){
+    this.poids = event.target.value
+  }
+  onKeyTwo(event: any){
+    this.taille = event.target.value
+  }
+  
 }
