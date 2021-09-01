2638e4c40b4d6e983e0f6a08850038287c1c6421
izno94 <iznosc2@gmail.com>
2021-09-01 12:22:25 +0200

    add path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 import { ImcCalComponent } from './imc-cal/imc-cal.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 
 const routes: Routes = [{ path: "imccalculator", component: ImcCalComponent,},
-                        { path:"todolist", component: ToDoListComponent}];
+                        { path:"todolist", component: ToDoListComponent},
+                        { path:"homepage", component: HomePageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
