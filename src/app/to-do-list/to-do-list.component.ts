14d641f75c099f1ce42f4a38097adaa118e8b1b7
Romain Herrrero <iznosc2@gmail.com>
2021-10-05 17:33:07 +0200

    create function add and deleted

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent implements OnInit {
+  public todo
+  public importData
 
-  constructor() { }
+  constructor() {
+    this.todo = []
+   }
 
   ngOnInit() {
   }
+add(){
+  this.todo.push(this.importData)
+  
+}
+delete(index){
+  this.todo.splice(index,1)
+}
 
 }
